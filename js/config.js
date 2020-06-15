2972b167f3c2cd328e8728b69bbb554c14bfca49
jtorre20 <jtorres1989@gmail.com>
2020-06-15 21:25:39 +0000

    updated userpoolid, appid, and region for cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_DuKC2TG04', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '252egsm1as5dutgdmckm7le1cg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
